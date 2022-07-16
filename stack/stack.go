11c8096a14819b339017f494502447acca2c7409
michelprogram <gaurondorian@gmail.com>
2022-07-16 17:23:19 +0000

    :sparkles: setup

---
+++ stack/stack.go
@@ -0,0 +1,38 @@
+package main
+
+type Stack[T any] struct {
+	arr  []T
+	top  T
+	size int
+}
+
+func NewStack(size int) {
+
+}
+
+//Add an element to the top of a stack
+
+func Push() {
+
+}
+
+//Remove an element from the top of a stack
+func Pop() {
+
+}
+
+//Check if the stack is empty
+
+func IsEmpty() {
+
+}
+
+//Check if the stack is full
+func IsFull() {
+
+}
+
+//Get the value of the top element without removing it
+func Peek() {
+
+}
