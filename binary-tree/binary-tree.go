622e1af7ba5b68703a917b52394ea95c6e5edf36
michelprogram <gaurondorian@gmail.com>
2022-07-20 11:28:26 +0000

    BST

---
+++ binary-tree/binary-tree.go
@@ -0,0 +1,320 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/exp/constraints"
+)
+
+type Node[T constraints.Ordered] struct {
+	data  T
+	left  *Node[T]
+	right *Node[T]
+}
+
+func NewNode[T constraints.Ordered](data T) *Node[T] {
+	node := Node[T]{
+		data:  data,
+		left:  nil,
+		right: nil,
+	}
+
+	return &node
+}
+
+func (n Node[T]) String() string {
+	return fmt.Sprintf("\nData : %v left : %v right :%v\n", n.data, n.left, n.right)
+}
+
+func (n Node[T]) hasOneChild() bool {
+	return (n.left == nil && n.right != nil) || (n.left != nil && n.right == nil)
+}
+
+func (n Node[T]) hasTwoChild() bool {
+	return n.left != nil && n.right != nil
+}
+
+func (n Node[T]) isALeaf() bool {
+	return n.left == nil && n.right == nil
+}
+
+//The Tree struct is not needed, but was really helpful in my mind :)
+type Tree[T constraints.Ordered] struct {
+	root *Node[T]
+}
+
+func NewTree[T constraints.Ordered](root *Node[T]) *Tree[T] {
+	tree := &Tree[T]{
+		root: root,
+	}
+
+	return tree
+}
+
+//NR -> None recursive
+func (t Tree[T]) SearchNR(looking T) bool {
+
+	node := t.root
+	flag := false
+
+	if node == nil {
+		return flag
+	}
+
+	for !flag && node != nil {
+
+		if looking == node.data {
+			return true
+		} else if looking < node.data {
+			node = node.left
+		} else {
+			node = node.right
+		}
+	}
+
+	return flag
+
+}
+
+//R -> Recursive
+func (t Tree[T]) SearchR(looking T) *Node[T] {
+
+	//Because i have created Tree struct
+	var search func(node *Node[T], looking T) *Node[T]
+
+	search = func(node *Node[T], looking T) *Node[T] {
+		if node == nil {
+			return nil
+		}
+
+		if node.data == looking {
+			return node
+		}
+
+		if looking < node.data {
+			return search(node.left, looking)
+		}
+
+		return search(node.right, looking)
+	}
+
+	return search(t.root, looking)
+
+}
+
+func (t Tree[T]) InsertNR(insert T) {
+
+	node := t.root
+
+	flag := false
+
+	for !flag {
+
+		if insert < node.data {
+			if node.left == nil {
+				node.left = NewNode(insert)
+				flag = true
+			}
+			node = node.left
+		} else {
+			if node.right == nil {
+				node.right = NewNode(insert)
+				flag = true
+			}
+			node = node.right
+		}
+	}
+
+}
+
+func (t Tree[T]) InsertR(value T) {
+
+	var insert func(node *Node[T], value T)
+
+	insert = func(node *Node[T], value T) {
+		if value < node.data {
+			if node.left == nil {
+				node.left = NewNode(value)
+				return
+			}
+			insert(node.left, value)
+		} else {
+			if node.right == nil {
+				node.right = NewNode(value)
+				return
+			}
+			insert(node.right, value)
+		}
+
+	}
+
+	insert(t.root, value)
+
+}
+
+func (t Tree[T]) CountNodeR() int {
+
+	var res int = 0
+
+	var traversal func(node *Node[T])
+
+	traversal = func(node *Node[T]) {
+		if node != nil {
+			res++
+			traversal(node.left)
+			traversal(node.right)
+		}
+	}
+
+	traversal(t.root)
+
+	return res
+
+}
+
+//TODO : upate code
+func (t Tree[T]) Delete(value T) {
+
+	if t.SearchR(value) != nil {
+
+		var parent, node *Node[T] = nil, t.root
+		flag := false
+
+		if t.IsEmpty() {
+			return
+		}
+
+		for !flag {
+
+			if value < node.data {
+				parent = node
+				node = node.left
+			} else if value > node.data {
+				parent = node
+				node = node.right
+			} else {
+
+				if node.isALeaf() {
+					//Case leaf node
+
+					if parent.left.data == node.data {
+						parent.left = nil
+					} else {
+						parent.right = nil
+					}
+
+				} else if node.hasOneChild() {
+					//Case one node has one child
+					var childOfNode *Node[T] = nil
+					
+					if node.left != nil {
+						childOfNode = node.left
+					} else {
+						childOfNode = node.right
+					}
+
+					//Side of previous node
+					if parent.left.data == node.data {
+						parent.left = childOfNode
+					} else {
+						parent.right = childOfNode
+					}
+				} else {
+					//Case left two child
+
+					i := t.Inorder(node)
+
+					t.Delete(i.data)
+
+					node.data = i.data
+
+				}
+
+				flag = true
+
+			}
+		}
+
+	}
+
+}
+
+/*
+else {
+					var childOfNode *Node[T] = nil
+
+					if node.hasOneChild() {
+						//Case one node has one child
+
+						if node.left != nil {
+							childOfNode = node.left
+						} else {
+							childOfNode = node.right
+						}
+					} else {
+						//Case left two child
+
+						i := t.Inorder(node)
+
+						fmt.Println(i)
+
+						if node.left.data > node.right.data {
+							childOfNode = node.left
+							childOfNode.left = node.left
+
+						} else {
+							childOfNode = node.right
+							childOfNode.left = node.left
+
+						}
+					}
+
+					//Side of previous node
+					if parent.left.data == node.data {
+						parent.left = childOfNode
+					} else {
+						parent.right = childOfNode
+					}
+				}
+*/
+
+func (t Tree[T]) GetParent(node *Node[T], value T) *Node[T] {
+
+	if node == nil {
+		return nil
+	}
+
+	var previous *Node[T] = nil
+
+	for node != nil {
+		if value < node.data {
+			previous = node
+			node = node.left
+		} else if value > node.data {
+			previous = node
+			node = node.right
+		} else {
+			node = nil
+		}
+	}
+
+	return previous
+}
+
+func (t Tree[T]) PeekRoot() *Node[T] {
+	return t.root
+}
+
+func (t Tree[T]) IsEmpty() bool {
+	return t.root == nil
+}
+
+func (t Tree[T]) Inorder(node *Node[T]) *Node[T] {
+
+	tree := node.right
+	for tree.left != nil {
+		tree = tree.left
+	}
+	return tree
+
+}
---
+++ binary-tree/binary-tree_test.go
@@ -0,0 +1,296 @@
+package main
+
+import (
+	"testing"
+)
+
+func BasicTree() *Tree[int] {
+
+	tree := NewTree(NewNode(1))
+
+	tree.root.left = NewNode(2)
+	tree.root.right = NewNode(3)
+
+	tree.root.left.left = NewNode(4)
+	tree.root.left.right = NewNode(5)
+
+	tree.root.left.right.left = NewNode(21)
+
+	return tree
+}
+
+func PerfectBinaryTree() *Tree[int] {
+
+	tree := NewTree(NewNode(8))
+
+	tree.root.left = NewNode(4)
+
+	tree.root.left.left = NewNode(2)
+
+	tree.root.left.left.left = NewNode(1)
+	tree.root.left.left.right = NewNode(3)
+
+	tree.root.left.right = NewNode(6)
+
+	tree.root.left.right.right = NewNode(7)
+	tree.root.left.right.left = NewNode(5)
+
+	tree.root.right = NewNode(12)
+
+	tree.root.right.left = NewNode(10)
+
+	tree.root.right.left.left = NewNode(9)
+	tree.root.right.left.right = NewNode(11)
+
+	tree.root.right.right = NewNode(14)
+
+	tree.root.right.right.right = NewNode(15)
+	tree.root.right.right.left = NewNode(13)
+
+	return tree
+
+}
+
+func TestNode(t *testing.T) {
+
+	node := NewNode(1)
+
+	node.left = NewNode(2)
+
+	node.left.left = NewNode(3)
+
+	if node.right != nil {
+		t.Errorf("Node right of root node was incorrect, got: %v, want: %v.", node.right, nil)
+
+	}
+
+	if node.left.left.data != 3 {
+		t.Errorf("Third level Node was incorrect, got: %v, want: %v.", node.left.left.data, 3)
+	}
+
+}
+
+func TestNodeIsLeaf(t *testing.T) {
+
+	tree := PerfectBinaryTree()
+
+	get := tree.SearchR(1)
+
+	if !get.isALeaf() {
+		t.Errorf("Is leaf was incorrect, got: %t, want: %t.", get.isALeaf(), true)
+	}
+
+	get = tree.SearchR(2)
+
+	if get.isALeaf() {
+		t.Errorf("Is leaf was incorrect, got: %t, want: %t.", get.isALeaf(), false)
+	}
+
+}
+
+func TestNodeHasOneChild(t *testing.T) {
+
+	tree := PerfectBinaryTree()
+
+	tree.InsertR(21)
+
+	get := tree.SearchR(15)
+
+	if !get.hasOneChild() {
+		t.Errorf("Has one child was incorrect, got: %t, want: %t.", get.hasOneChild(), true)
+	}
+
+	get = tree.SearchR(2)
+
+	if get.hasOneChild() {
+		t.Errorf("has one child was incorrect, got: %t, want: %t.", get.hasOneChild(), false)
+	}
+
+}
+
+func TestNodeHasTwoChild(t *testing.T) {
+
+	tree := PerfectBinaryTree()
+
+	get := tree.SearchR(14)
+
+	if !get.hasTwoChild() {
+		t.Errorf("Has two child was incorrect, got: %t, want: %t.", get.hasOneChild(), true)
+	}
+
+	get = tree.SearchR(2)
+
+	if get.hasOneChild() {
+		t.Errorf("has two child was incorrect, got: %t, want: %t.", get.hasOneChild(), false)
+	}
+
+}
+
+func TestTree(t *testing.T) {
+
+	tree := NewTree(NewNode(1))
+
+	if tree.root.data != 1 {
+		t.Errorf("Tree root Node was incorrect, got: %v, want: %v.", tree.root.data, 1)
+	}
+
+}
+
+func TestSearchNR(t *testing.T) {
+
+	tree := PerfectBinaryTree()
+
+	if !tree.SearchNR(1) {
+		t.Errorf("Search NR was incorrect, got: %t, want: %t.", tree.SearchNR(1), true)
+	}
+
+	if tree.SearchNR(21) {
+		t.Errorf("Search NR was incorrect, got: %t, want: %t.", tree.SearchNR(21), false)
+	}
+}
+
+func TestPeekRoot(t *testing.T) {
+
+	tree := PerfectBinaryTree()
+
+	if tree.PeekRoot().data != 8 {
+		t.Errorf("Peek root was incorrect, got: %v, want: %v.", tree.PeekRoot().data, 8)
+	}
+
+}
+
+func TestSearchR(t *testing.T) {
+
+	tree := PerfectBinaryTree()
+
+	if tree.SearchR(1) == nil {
+		t.Errorf("Search R was incorrect, got: %v, want: %s.", tree.SearchR(1), "not nil")
+	}
+
+	if tree.SearchR(21) != nil {
+		t.Errorf("Search R was incorrect, got: %v, want: %v.", tree.SearchR(21), nil)
+	}
+}
+
+func TestInsertNR(t *testing.T) {
+
+	tree := PerfectBinaryTree()
+
+	tree.InsertNR(21)
+
+	if tree.SearchR(21) == nil {
+		t.Errorf("InsertNR was incorrect, got: %v, want: %s.", tree.SearchR(21), "not nil")
+	}
+
+}
+
+func TestInsertR(t *testing.T) {
+
+	tree := PerfectBinaryTree()
+
+	tree.InsertR(21)
+
+	if tree.SearchR(21) == nil {
+		t.Errorf("Insert R was incorrect, got: %v, want: %v.", tree.SearchR(21), "not nil")
+	}
+
+}
+
+func TestCountNode(t *testing.T) {
+
+	tree := PerfectBinaryTree()
+
+	lenBase := tree.CountNodeR()
+
+	if lenBase != 15 {
+		t.Errorf("Count was incorrect, got: %d, want: %d.", lenBase, 15)
+	}
+
+	tree.InsertR(21)
+
+	if tree.CountNodeR() != lenBase+1 {
+		t.Errorf("Count was incorrect, got: %d, want: %d.", tree.CountNodeR(), lenBase+1)
+
+	}
+
+}
+
+func TestParentNode(t *testing.T) {
+
+	tree := PerfectBinaryTree()
+
+	get := tree.GetParent(tree.root, 3).data
+
+	if get != 2 {
+		t.Errorf("Get parent was incorrect, got: %v, want: %d.", get, 2)
+	}
+
+}
+
+func TestRemoveNodeLeaf(t *testing.T) {
+
+	tree := PerfectBinaryTree()
+
+	tree.Delete(1)
+
+	if tree.SearchR(1) != nil {
+		t.Errorf("Remove leaf node was incorrect, got: %v, want: %v.", tree.SearchR(1), nil)
+	}
+
+}
+
+func TestRemoveNodeOneChild(t *testing.T) {
+
+	tree := PerfectBinaryTree()
+
+	tree.InsertR(21)
+
+	tree.Delete(15)
+
+	if tree.SearchR(14).right.data != 21 {
+		t.Errorf("Remove node with one child was incorrect, got: %v, want: %v.", tree.SearchR(14).right.data, 21)
+	}
+
+}
+
+func TestRemoveNodeOneChild2(t *testing.T) {
+
+	tree := PerfectBinaryTree()
+
+	tree.InsertR(21)
+	tree.InsertR(19)
+
+	tree.InsertR(25)
+	tree.InsertR(23)
+
+	tree.Delete(15)
+
+	if tree.SearchR(14).right.right.data != 25 {
+		t.Errorf("Remove node with one child was incorrect, got: %v, want: %v.", tree.SearchR(14).right.right.data, 25)
+	}
+
+}
+
+func TestRemoveNodeTwoChild(t *testing.T) {
+
+	tree := PerfectBinaryTree()
+
+	tree.Delete(10)
+
+	if tree.SearchR(10) != nil {
+		t.Errorf("Remove node with two child was incorrect, got: %v, want: %v.", tree.SearchR(10), nil)
+	}
+
+}
+
+func TestRemoveNodeTwoChild2(t *testing.T) {
+
+	tree := PerfectBinaryTree()
+
+	tree.Delete(12)
+
+	if tree.SearchR(14).left != nil {
+		t.Errorf("Remove node with two child was incorrect, got: %v, want: %v.", tree.SearchR(12), nil)
+	}
+
+}
